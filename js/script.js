0f7b98fe94747a19beb10bd7f0720fea7726829e
Jenny Zuniga <jmzuniga@hawaii.edu>
2023-04-04 07:34:19 -1000

    updated site

--- js/script.js
+++ js/script.js
@@ -20,3 +20,76 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+
+
+
+
+
+
+// FOR INDEX.HTML:
+// <swiper-container class="mySwiper" navigation="true" pagination="true" keyboard="true" mousewheel="true"
+// css-mode="true">
+//     <swiper-slide>
+//         <img src="img/SVG/moth.svg" alt="Photo of Jenny">
+//     </swiper-slide>
+//     <swiper-slide>
+//         <img src="img/SVG/pearl.svg" alt="Photo of Jenny">
+//     </swiper-slide>
+//     <swiper-slide>
+//         <img src="img/SVG/wolf.svg" alt="Photo of Jenny">
+//     </swiper-slide>
+//     <swiper-slide>Slide 4</swiper-slide>
+//     <swiper-slide>Slide 5</swiper-slide>
+//     <swiper-slide>Slide 6</swiper-slide>
+//     <swiper-slide>Slide 7</swiper-slide>
+//     <swiper-slide>Slide 8</swiper-slide>
+//     <swiper-slide>Slide 9</swiper-slide>
+// </swiper-container>
+
+// <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+
+// FOR SCRIPT JS:
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+      
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+      
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+      
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
+
+// FOR CSS:
+// swiper-container {
+//     width: 100%;
+//     height: 95%;
+//   }
+
+// swiper-slide {
+//     text-align: center;
+//     font-size: 18px;
+//     background: #fff;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+
+//     img {
+//         display: block;
+//         width: 100%;
+//         height: 90%;
+//         object-fit: cover;
+//     }
+// }
