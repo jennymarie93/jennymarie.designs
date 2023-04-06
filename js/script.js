a913186f2e3097b0d89f9d131531aba2819584db
Jenny Zuniga <jmzuniga@hawaii.edu>
2023-04-06 08:22:41 -1000

    added animation on scroll

--- js/script.js
+++ js/script.js
@@ -21,75 +21,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+  
+
+
+// REVEAL ON SCROLL JS
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute('data-sectionstate', 'active');
+        } else {
+            entry.target.setAttribute('data-sectionstate', 'inactive');
+        }
+    });
+});
+document.querySelectorAll('.animate-on-scroll').forEach((section) => {
+    myobserver.observe(section);
+});
 
 
 
 
 
-
-// FOR INDEX.HTML:
-// <swiper-container class="mySwiper" navigation="true" pagination="true" keyboard="true" mousewheel="true"
-// css-mode="true">
-//     <swiper-slide>
-//         <img src="img/SVG/moth.svg" alt="Photo of Jenny">
-//     </swiper-slide>
-//     <swiper-slide>
-//         <img src="img/SVG/pearl.svg" alt="Photo of Jenny">
-//     </swiper-slide>
-//     <swiper-slide>
-//         <img src="img/SVG/wolf.svg" alt="Photo of Jenny">
-//     </swiper-slide>
-//     <swiper-slide>Slide 4</swiper-slide>
-//     <swiper-slide>Slide 5</swiper-slide>
-//     <swiper-slide>Slide 6</swiper-slide>
-//     <swiper-slide>Slide 7</swiper-slide>
-//     <swiper-slide>Slide 8</swiper-slide>
-//     <swiper-slide>Slide 9</swiper-slide>
-// </swiper-container>
-
-// <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
-
-// FOR SCRIPT JS:
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     loop: true,
-      
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//     },
-      
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-      
-//     // And if we need scrollbar
-//     scrollbar: {
-//         el: '.swiper-scrollbar',
-//     },
-// });
-
-// FOR CSS:
-// swiper-container {
-//     width: 100%;
-//     height: 95%;
-//   }
-
-// swiper-slide {
-//     text-align: center;
-//     font-size: 18px;
-//     background: #fff;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-
-//     img {
-//         display: block;
-//         width: 100%;
-//         height: 90%;
-//         object-fit: cover;
-//     }
-// }
