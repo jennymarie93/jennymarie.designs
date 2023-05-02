25074d1de883903d1230a1e89228433c976b33e7
Jenny Zuniga <jmzuniga@hawaii.edu>
2023-05-02 09:32:15 -1000

    modified nav

--- js/script.js
+++ js/script.js
@@ -17,28 +17,49 @@ document.addEventListener('DOMContentLoaded', function() {
         };
     };
 
+    // STICKY NAV SCROLL LINKS FOR SINGLE PAGE SITES
+    var smallnavlinks = document.querySelectorAll(".site-header-small nav a");
+    var smallheader = document.querySelector(".site-header-small .site-nav");
+    var j;
+    for (j = 0; j < smallnavlinks.length; j++) {
+        smallnavlinks[j].onclick = function () {
+            console.log("click");
+            smallheader.setAttribute('data-navstate', 'closed');
+        };
+    };
+
+    
+    // REVEAL ON SCROLL JS
+    // CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+    const myobserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.setAttribute('data-sectionstate', 'active');
+            } else {
+                entry.target.setAttribute('data-sectionstate', 'inactive');
+            }
+        });
+    });
+    document.querySelectorAll('.animate-on-scroll').forEach((section) => {
+        myobserver.observe(section);
+    });
+
+    // SCROLL STATE
+
+    var offsetStart = 0;
+    var offsetEnd = 0;
+
+    window.addEventListener('scroll', () => {
+    document.documentElement.style.setProperty('--scroll', ( window.pageYOffset - offsetStart ) / ( document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight ));
+    }, false);
+
+
+
 });
 
 
 
   
-
-
-// REVEAL ON SCROLL JS
-// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
-const myobserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.setAttribute('data-sectionstate', 'active');
-        } else {
-            entry.target.setAttribute('data-sectionstate', 'inactive');
-        }
-    });
-});
-document.querySelectorAll('.animate-on-scroll').forEach((section) => {
-    myobserver.observe(section);
-});
-
 
 
 
